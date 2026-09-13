1477e654ef4b69eb4ab5d5c297a94e8ab02a8c34
brittgalloway <crlnfllr@gmail.com>
2026-09-13 14:58:55 -0500

    updated the cart to a dialog

--- src/javascript/components/site-cart.js
+++ src/javascript/components/site-cart.js
@@ -6,6 +6,24 @@ template.innerHTML = markup;
 class SiteCart  extends HTMLElement {
     connectedCallback() {
         this.appendChild(template.content.cloneNode(true));
+
+        this.dialog = this.querySelector('#site-cart-dialog');
+        if (!this.dialog) return;
+
+        const closeBtn = this.querySelector('#site-ccart-close');
+        closeBtn?.addEventListener('click', () => this.close());
+
+        this.dialog.addEventListener('click', (e) => {
+            if (e.target === this.dialog) {
+                this.close();
+            }
+        });
+    }
+    open() {
+        this.dialog?.showModal();
+    }
+    close() {
+        this.dialog?.close();
     }
 }
 
